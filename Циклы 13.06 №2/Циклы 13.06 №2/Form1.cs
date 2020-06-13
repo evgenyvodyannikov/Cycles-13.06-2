8d4f208e417303e20aaacba1768e7e1d8d8141d8
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-13 13:48:09 +0500

    небольшние преды

--- Циклы 13.06 №2/Циклы 13.06 №2/Form1.cs
+++ Циклы 13.06 №2/Циклы 13.06 №2/Form1.cs
@@ -22,33 +22,39 @@ namespace Циклы_13._06__2
 
             // обьявление переменных
             TimeSpan startDay = TimeSpan.Parse(maskedTextBox1.Text);
-            TimeSpan lessonDuration = TimeSpan.Parse(maskedTextBox2.Text);
-            TimeSpan StandartInterval = TimeSpan.Parse(maskedTextBox3.Text);
-            TimeSpan BigInterval = TimeSpan.Parse(maskedTextBox4.Text);
-            int indexBigInterval = (int)numericUpDown1.Value;
-            int countLessons = (int)numericUpDown2.Value;
-            Console.WriteLine();
-
-            TimeSpan time = startDay;
-            for (int i = 1; i <= countLessons; i++) // цикл с 1 по последний урок
+            if (startDay >= TimeSpan.Parse("17:00") || startDay <= TimeSpan.Parse("07:00"))
             {
-                listBox1.Items.Add("Урок " + i); // записываем № урока
-                listBox1.Items.Add(time); // время нач. урока
-                time = time.Add(lessonDuration); // добавляем к переменной time продолжительность урока
-                listBox1.Items.Add(time); // теперь уже будет выведено нач. урока + продолжительность, то есть конец урока
-                if (i != countLessons) // проверяем не последний ли урок?
+                MessageBox.Show("По рекомаендациям Министерства Образования РФ начало занятий до 7 часов утра запрещено, также как и занятия после 17:00");
+            }
+            else
+            {
+                TimeSpan lessonDuration = TimeSpan.Parse(maskedTextBox2.Text);
+                TimeSpan StandartInterval = TimeSpan.Parse(maskedTextBox3.Text);
+                TimeSpan BigInterval = TimeSpan.Parse(maskedTextBox4.Text);
+                int indexBigInterval = (int)numericUpDown1.Value;
+                int countLessons = (int)numericUpDown2.Value;
+
+                TimeSpan time = startDay;
+                for (int i = 1; i <= countLessons; i++) // цикл с 1 по последний урок
                 {
-                    listBox1.Items.Add("Перерыв:"); // добавляем надпись "перерыв"
-                    listBox1.Items.Add(time); // записываем время начала
-                    if (i != indexBigInterval) // проверяем большой ли сейчас перерыв?
+                    listBox1.Items.Add("Урок " + i); // записываем № урока
+                    listBox1.Items.Add(time); // время нач. урока
+                    time = time.Add(lessonDuration); // добавляем к переменной time продолжительность урока
+                    listBox1.Items.Add(time); // теперь уже будет выведено нач. урока + продолжительность, то есть конец урока
+                    if (i != countLessons) // проверяем не последний ли урок?
                     {
-                        time = time.Add(StandartInterval); // добавляем продолжительность обычного перерыва
-                        listBox1.Items.Add(time); // записываем конец перерыва
-                    }
-                    else // то же самое, но для большого перерыва
-                    {
-                        time = time.Add(BigInterval);
-                        listBox1.Items.Add(time);
+                        listBox1.Items.Add("Перерыв:"); // добавляем надпись "перерыв"
+                        listBox1.Items.Add(time); // записываем время начала
+                        if (i != indexBigInterval) // проверяем большой ли сейчас перерыв?
+                        {
+                            time = time.Add(StandartInterval); // добавляем продолжительность обычного перерыва
+                            listBox1.Items.Add(time); // записываем конец перерыва
+                        }
+                        else // то же самое, но для большого перерыва
+                        {
+                            time = time.Add(BigInterval);
+                            listBox1.Items.Add(time);
+                        }
                     }
                 }
             }
