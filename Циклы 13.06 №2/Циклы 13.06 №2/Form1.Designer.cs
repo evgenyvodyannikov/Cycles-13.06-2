dc97f7b52377e8333e26fe401ae3fa8924a20051
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-13 14:27:33 +0500

    Финал

--- Циклы 13.06 №2/Циклы 13.06 №2/Form1.Designer.cs
+++ Циклы 13.06 №2/Циклы 13.06 №2/Form1.Designer.cs
@@ -31,43 +31,50 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
-            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.label5 = new System.Windows.Forms.Label();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(403, 272);
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(554, 310);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(141, 55);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Расчитать";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(389, 12);
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(389, 25);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(210, 173);
+            this.listBox1.Size = new System.Drawing.Size(399, 279);
             this.listBox1.TabIndex = 1;
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.groupBox1.Controls.Add(this.numericUpDown2);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.numericUpDown1);
@@ -81,100 +88,12 @@
             this.groupBox1.Controls.Add(this.maskedTextBox2);
             this.groupBox1.Controls.Add(this.maskedTextBox1);
             this.groupBox1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(12, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(328, 292);
+            this.groupBox1.Size = new System.Drawing.Size(361, 300);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Расписание звонков";
-            // 
-            // maskedTextBox1
-            // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(164, 40);
-            this.maskedTextBox1.Mask = "90:00";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(45, 33);
-            this.maskedTextBox1.TabIndex = 5;
-            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 43);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(152, 25);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Начало уч. дня:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 79);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(120, 25);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Прод. пары:";
-            // 
-            // maskedTextBox2
-            // 
-            this.maskedTextBox2.Location = new System.Drawing.Point(132, 76);
-            this.maskedTextBox2.Mask = "90:00";
-            this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(45, 33);
-            this.maskedTextBox2.TabIndex = 5;
-            this.maskedTextBox2.ValidatingType = typeof(System.DateTime);
-            // 
-            // maskedTextBox3
-            // 
-            this.maskedTextBox3.Location = new System.Drawing.Point(267, 109);
-            this.maskedTextBox3.Mask = "90:00";
-            this.maskedTextBox3.Name = "maskedTextBox3";
-            this.maskedTextBox3.Size = new System.Drawing.Size(45, 33);
-            this.maskedTextBox3.TabIndex = 5;
-            this.maskedTextBox3.ValidatingType = typeof(System.DateTime);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 112);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(255, 25);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Прод. обычного перерыва:";
-            // 
-            // maskedTextBox4
-            // 
-            this.maskedTextBox4.Location = new System.Drawing.Point(267, 145);
-            this.maskedTextBox4.Mask = "90:00";
-            this.maskedTextBox4.Name = "maskedTextBox4";
-            this.maskedTextBox4.Size = new System.Drawing.Size(45, 33);
-            this.maskedTextBox4.TabIndex = 5;
-            this.maskedTextBox4.ValidatingType = typeof(System.DateTime);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 148);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(255, 25);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Прод. большого перерыва:";
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(211, 182);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(45, 33);
-            this.numericUpDown1.TabIndex = 7;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 184);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(199, 25);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "Место в расписании:";
             // 
             // numericUpDown2
             // 
@@ -192,20 +111,126 @@
             this.label6.TabIndex = 6;
             this.label6.Text = "Кол-во уроков:";
             // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(211, 182);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(45, 33);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(199, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Место в расписании:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(255, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Прод. большого перерыва:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(255, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Прод. обычного перерыва:";
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(267, 145);
+            this.maskedTextBox4.Mask = "90:00";
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(60, 33);
+            this.maskedTextBox4.TabIndex = 5;
+            this.maskedTextBox4.ValidatingType = typeof(System.DateTime);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Прод. пары:";
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(267, 109);
+            this.maskedTextBox3.Mask = "90:00";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(60, 33);
+            this.maskedTextBox3.TabIndex = 5;
+            this.maskedTextBox3.ValidatingType = typeof(System.DateTime);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Начало уч. дня:";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(132, 76);
+            this.maskedTextBox2.Mask = "90:00";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(54, 33);
+            this.maskedTextBox2.TabIndex = 5;
+            this.maskedTextBox2.ValidatingType = typeof(System.DateTime);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(164, 40);
+            this.maskedTextBox1.Mask = "90:00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(54, 33);
+            this.maskedTextBox1.TabIndex = 5;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.BackgroundImage = global::Циклы_13._06__2.Properties.Resources.q;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(701, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 54);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(792, 369);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Водянников 13.06 Задание 8";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -227,6 +252,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Циклы 13.06 №2/Циклы 13.06 №2/Form1.cs
+++ Циклы 13.06 №2/Циклы 13.06 №2/Form1.cs
@@ -19,45 +19,68 @@ namespace Циклы_13._06__2
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            listBox1.Items.Clear();
             // обьявление переменных
-            TimeSpan startDay = TimeSpan.Parse(maskedTextBox1.Text);
-            if (startDay >= TimeSpan.Parse("17:00") || startDay <= TimeSpan.Parse("07:00"))
+            try
             {
-                MessageBox.Show("По рекомаендациям Министерства Образования РФ начало занятий до 7 часов утра запрещено, также как и занятия после 17:00");
-            }
-            else
-            {
+                TimeSpan startDay = TimeSpan.Parse(maskedTextBox1.Text);
                 TimeSpan lessonDuration = TimeSpan.Parse(maskedTextBox2.Text);
                 TimeSpan StandartInterval = TimeSpan.Parse(maskedTextBox3.Text);
                 TimeSpan BigInterval = TimeSpan.Parse(maskedTextBox4.Text);
                 int indexBigInterval = (int)numericUpDown1.Value;
                 int countLessons = (int)numericUpDown2.Value;
 
-                TimeSpan time = startDay;
-                for (int i = 1; i <= countLessons; i++) // цикл с 1 по последний урок
+
+                if (startDay >= TimeSpan.Parse("17:00") || startDay <= TimeSpan.Parse("07:00"))
                 {
-                    listBox1.Items.Add("Урок " + i); // записываем № урока
-                    listBox1.Items.Add(time); // время нач. урока
-                    time = time.Add(lessonDuration); // добавляем к переменной time продолжительность урока
-                    listBox1.Items.Add(time); // теперь уже будет выведено нач. урока + продолжительность, то есть конец урока
-                    if (i != countLessons) // проверяем не последний ли урок?
+                    MessageBox.Show("По рекомаендациям Министерства Образования РФ начало занятий до 7 часов утра запрещено, также как и занятия после 17:00", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (StandartInterval <= TimeSpan.Parse("00:05") || BigInterval <= TimeSpan.Parse("00:30"))
+                {
+                    MessageBox.Show("По рекомаендациям Министерства Образования РФ продолжительность большого и обычного перерыва должна быть не меньше 5 и 30 минут соответсвенно.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else if (countLessons >= 8)
+                {
+                    MessageBox.Show("По рекомаендациям Министерства Образования РФ кол-во занятий не должно превышать 7.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    TimeSpan time = startDay;
+                    for (int i = 1; i <= countLessons; i++) // цикл с 1 по последний урок
                     {
-                        listBox1.Items.Add("Перерыв:"); // добавляем надпись "перерыв"
-                        listBox1.Items.Add(time); // записываем время начала
-                        if (i != indexBigInterval) // проверяем большой ли сейчас перерыв?
+                        listBox1.Items.Add("Урок " + i); // записываем № урока
+                        listBox1.Items.Add(time); // время нач. урока
+                        time = time.Add(lessonDuration); // добавляем к переменной time продолжительность урока
+                        listBox1.Items.Add(time); // теперь уже будет выведено нач. урока + продолжительность, то есть конец урока
+                        if (i != countLessons) // проверяем не последний ли урок?
                         {
-                            time = time.Add(StandartInterval); // добавляем продолжительность обычного перерыва
-                            listBox1.Items.Add(time); // записываем конец перерыва
+                            listBox1.Items.Add("Перерыв:"); // добавляем надпись "перерыв"
+                            listBox1.Items.Add(time); // записываем время начала
+                            if (i != indexBigInterval) // проверяем большой ли сейчас перерыв?
+                            {
+                                time = time.Add(StandartInterval); // добавляем продолжительность обычного перерыва
+                                listBox1.Items.Add(time); // записываем конец перерыва
+                            }
+                            else // то же самое, но для большого перерыва
+                            {
+                                time = time.Add(BigInterval);
+                                listBox1.Items.Add(time);
+                            }
                         }
-                        else // то же самое, но для большого перерыва
+                        if (time > TimeSpan.Parse("17:00"))
                         {
-                            time = time.Add(BigInterval);
-                            listBox1.Items.Add(time);
+                            MessageBox.Show("Внимание! Занятия проходят после 17:00.\nПересмотрите расписание.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                         }
                     }
                 }
             }
+            catch {
+                MessageBox.Show("Проверьте правильность ввода данных.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Расписание звонков. В учебном заведении задается начало учебного дня, продолжительность «пары» или урока, продолжительность обычного и большого перерывов (и их «место» в расписании), количество пар (уроков). Получить расписание звонков на весь учебный день.", "Задание", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
